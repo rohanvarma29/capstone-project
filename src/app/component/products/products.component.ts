e322a0ac0e0278c7fbbfa410429b2eedd3df8228
LAPTOP-FKD5OKLN\rohan <rohangaddam95@gmail.com>
2022-07-19 15:02:20 +0530

    linked with micro-service

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -20,9 +20,6 @@ export class ProductsComponent implements OnInit {
       this.productList = res;
       this.filterCategory = res;
       this.productList.forEach((a:any) => {
-        if(a.category ==="women's clothing" || a.category ==="men's clothing"){
-          a.category ="fashion"
-        }
         Object.assign(a,{quantity:1,total:a.price});
       });
       console.log(this.productList)
@@ -36,12 +33,16 @@ export class ProductsComponent implements OnInit {
     this.cartService.addtoCart(item);
   }
   filter(category:string){
+    console.log(category);
+    console.log(this.productList);
     this.filterCategory = this.productList
     .filter((a:any)=>{
       if(a.category == category || category==''){
+        //console.log(a);
         return a;
       }
     })
+    console.log(this.filterCategory);
   }
 
 }
